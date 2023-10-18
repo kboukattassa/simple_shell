b1952462900a9501d9ab6ecf86df8b781a1b4fa1
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-10-18 11:10:42 +0000

    created a small shell

---
+++ shell.c
@@ -0,0 +1,106 @@
+#include "sshell.h"
+/**
+ *
+ *
+ */
+
+
+/* gets the path and returns it */
+char *_getenv(const char *env_var)
+{
+	extern char **environ; /* evironment vars*/
+	int i = 0; /*index*/
+	char *key;/*var to search*/
+
+	while (environ[i])
+	{
+		key = strtok(environ[i], "=");/*to check if the thats the item we looking for*/
+		if (strcmp(env_var, key) == 0) /*compares the env var with the key, if its the same*/
+			return (strtok(NULL, "\n")); /*if yes, continue*/
+		i++;
+	}
+	return (NULL);/* if env var not founds return null*/
+}
+
+/*returns the whole path for the command*/
+char *get_cmmd(char *cmmd)
+{
+	char *path = _getenv("PATH");/*gets the path*/
+	char *token;/*look for char by char*/
+	char *cmd_full;/*the full path of the command to be returned*/
+	struct stat st;/*check if the fun exists or no*/
+
+	token = strtok(path, ":");/*tokenize the string*/
+	while (token)
+	{
+		cmd_full = malloc(strlen(token) + strlen(cmmd) + 2);/*allocate memory to the command string*/
+		strcpy(cmd_full, token);/*copy the token to the path*/
+		strcat(cmd_full, "/");/*pate / to the command path*/
+		strcat(cmd_full, cmmd);/*paste the command of the user to the command path */
+		if (stat(cmd_full, &st) == 0)/*the command path exists or not*/
+			return (cmd_full);/*if yes, returns the path*/
+		free(cmd_full);/*otherwise frees the command path*/
+		token = strtok(NULL, ":");/*continues again*/
+	}
+	return (NULL);/*in case the command is not found*/
+}
+
+/*splits the input into commands & arguments*/
+char **split_string(char *buffer, char *del)
+{
+	char **tokens;/*will hold all the tokens*/
+	char *token;/*takes one by one*/
+	int i = 0;/*index to traverse all the tokens*/
+
+	tokens = malloc(sizeof(char*) * 1024);/*alloate memory to the tokens*/
+	token = strtok(buffer, del);/*tokenize*/
+	while (token)
+	{
+		tokens[i] = token;/*will put token in the first index of the array of tokens*/
+		token = strtok(NULL, del);/*the new token*/
+		i++;
+	}
+	tokens[i] = NULL;/*finish with null*/
+	return (tokens);/*returns tokens*/
+}
+
+int main(int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+
+	char *buffer = NULL; /*buffer*/
+	size_t buffer_size;/*buffer size*/
+	char *cmd; /*command to be stored*/
+	char **args;/*arguments*/
+	pid_t pid;/*process to be forked*/
+	int status, n_chars;
+
+	while (1)
+	{
+		write(1, "$ ", 2);/*prints the prompt*/
+		n_chars = getline(&buffer, &buffer_size, stdin);/*reads input from user*/
+		if (n_chars == -1)
+		{
+			write(1, "\n", 1);/*write a new line and exits*/
+			exit(1);
+		}
+		args = split_string(buffer, " \t\n");/*tokenized*/
+		if (strcmp(args[0], "exit") == 0)/*if first input is exit*/
+			exit(0);
+		pid = fork();/*create child process*/
+		if (pid == 0)/*if child process*/
+		{
+			cmd = get_cmmd(args[0]);/*the command written by the user*/
+			if (cmd)/*if command exist, then execute it*/
+				execve(cmd, args, env);
+			else
+				printf("command not found\n");
+			exit(0);
+		}
+		else/*if parent, then wait for child to finish*/
+			wait(&status);
+	}
+	return (0);
+
+}
--- sshell.h
+++ sshell.h
@@ -1,3 +1,17 @@
 #ifndef _SIMPLE_SHELL_H
 #define _SIMPLE_SHELL_H
+#define _GNU_SOURCE
+/* Header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/* Prototypes */
+void d_prompt(void);
+void read_in(char *cmd, size_t size);
+void exe_cmd(const char *cmd);
 #endif
